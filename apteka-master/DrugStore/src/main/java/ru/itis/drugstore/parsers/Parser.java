fa3bc0f563c74df8df34da590b0d41ce44b0bd7c
Daniil Vdovinov <d.y2010@mail.ru>
2019-07-27 16:19:15 +0300

    cleaning

--- apteka-master/DrugStore/src/main/java/ru/itis/drugstore/parsers/Parser.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.drugstore.parsers;
-
-import ru.itis.drugstore.models.Item;
-
-import java.util.List;
-
-public interface Parser {
-    List<Item> getItemsListByRequest(String request);
-}
